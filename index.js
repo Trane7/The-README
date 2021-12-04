6a35913c85895d44cd2d4b64efe10c75aeec985f
Dee <dgrowette@gmail.com>
2021-12-04 12:40:33 -0800

    added some questions to index.js

--- index.js
+++ index.js
@@ -1,7 +1,69 @@
 // TODO: Include packages needed for this application
-
+const fileGenerator
+const fs = require('fs')
+const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Title of your repository?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true
+            } else {
+                console.log('Please enter your repository name!')
+                return false
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confrimAbout',
+        message: 'Would you like to enter "installation instructions" or a link to a video?',
+        default: true
+        
+    },
+    {
+        type: 'confirm',
+        message: 'What kind of license does your project have?',
+        choices: [
+            'APACHE 2.0',
+            'BSD 3',
+            'gvl-gpl 3.0',
+            'MIT',
+            'NONE'
+        ],
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter your GitHub Username',
+        validate: usernameInput => {
+            if(usernameInput) {
+                return true
+            } else {
+                console.log('Please enter your GitHub Username!')
+                return false
+            }
+        }
+        
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter your Email Address',
+        validate: emailInput => {
+            if(emailInput) {
+                return true
+            } else {
+                console.log('Please enter Email Address!')
+                return false
+            }
+        }
+        
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
